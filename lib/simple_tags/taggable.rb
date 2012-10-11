c89122552bf6de6e4aa6dd8e84a875f66832d0a6
Tiago Scolari <tscolari@gmail.com>
2012-10-10 23:36:23 -0300

    Adding documentation

--- lib/simple_tags/taggable.rb
+++ lib/simple_tags/taggable.rb
@@ -5,11 +5,17 @@ module SimpleTags
     included do
       relation_name = self.name.underscore
       relation_class_name = self.name
+
+      # Adds a relation in the Tag model for listing this type of taggables
+      #
       SimpleTags::Tag.class_eval do
         has_many "#{relation_name}_taggings".to_sym, class_name: 'SimpleTags::Tagging', conditions: ["taggable_type = ?", relation_class_name], foreign_key: 'tag_id'
         has_many relation_name.pluralize.to_sym, class_name: relation_class_name, through: "#{relation_name}_taggings".to_sym, source: "#{relation_name}_filter".to_sym
       end
 
+      # Adds a relation in the Tagging model for listing this type of taggables
+      # This relation exists only to serve the Tag model relation for this object
+      #
       SimpleTags::Tagging.class_eval do
         belongs_to "#{relation_name}_filter".to_sym, class_name: relation_class_name, foreign_key: 'taggable_id'
       end
@@ -18,10 +24,15 @@ module SimpleTags
       has_many :tags, through: :taggings, class_name: 'SimpleTags::Tag'
     end
 
+    # Public: Return a comma-separated list of this model tags name
+    #
     def tag_list
       tags.map(&:name).join(", ")
     end
 
+    # Public: Sets the model tags
+    # `names` must be a list of tag names, separated by commas
+    #
     def tag_list=(names)
       self.tags = names.split(",").map do |n|
         SimpleTags::Tag.where(name: n.strip).first_or_create!
